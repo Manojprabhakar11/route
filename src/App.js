0c5b7e482c3df2abf96747e11d3201134922049e
Manoj prabhakar <manojprabhakar2311@gmail.com>
2022-12-29 10:51:50 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter} from "react-router-dom"
+import Route from "./routes/Route.js"
+
+
+function App() {
+  return (
+    <Route/> 
+  );
+}
+
+export default App;
